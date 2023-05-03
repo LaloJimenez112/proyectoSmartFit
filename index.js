bbd7872adfea7ece980fb6f990c0fd9113c6122d
Carlos Bautista <caedbape@gmail.com>
2023-05-02 23:39:10 -0600

    El Backend ya puede consultar si existe o no un usuario con determinada contraseña, falta que podamos enviarle una respuesta al frontend para que pueda hacer algo con ella.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const cors = require('cors')
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'master',
+  password: '12345',
+  database: 'GYM'
+});
 
 const app = express()
 app.use(cors())
@@ -11,6 +19,21 @@ app.post("/login/:usuarioId/:password", (req, res) => {
     const password = req.params.password || ""
 
     console.log(usuarioId, password);
+
+    connection.connect((error) => {
+        if (error) {
+          console.error('Error de conexión: ' + error.stack);
+          return;
+        }
+        console.log('Conexión a la base de datos exitosa');
+    });
+
+    connection.query(`SELECT usuarioUsuario, contrasenaUsuario FROM usuario WHERE usuarioUsuario = '${usuarioId}' && contrasenaUsuario = '${password}'`, function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+    });
+
+    connection.end()
 
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.end()
