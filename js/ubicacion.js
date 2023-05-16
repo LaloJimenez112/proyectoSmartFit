fed0373d7bddfc7126e6c4983441f4e82ded2f5b
Carlos Bautista <caedbape@gmail.com>
2023-05-15 19:32:43 -0600

    Cambios en las librerias Asicronia de las funciones

--- js/ubicacion.js
+++ js/ubicacion.js
@@ -8,63 +8,27 @@ let userLocation;
 
 //LISTA DE GIMNASIOS EN LA CDMX
 const gimnasiosIds = [
-
-  "ChIJXQvClg8QzYURAZxyA4ewNfw", //Sana fe ...
-  "ChIJtbvN6D8B0oUR5gZ4rbPbi9o",
-  "ChIJ0w-Qhq3_0YURVJj7TgjzgOw",
-  "ChIJOTKpqhcF0YUR5g7Qo1K3Tpk",
-  "ChIJXezCEZbh0YUR7I0CvssnJ8c",
-  "ChIJoSaKNKJn0YURva8Jmby7ld4",
-  "ChIJq35ZWHoJ0YURnO9b-d0JWLU",
-  "ChIJLcNUXYwU0YUR9A8WjKU5tug",
-  "ChIJC9ndN3hC0oURQTflgXuvmHo",
-  "ChIJkSDRvafv0YURtjquS_ADF6s",
-  "ChIJfQxv9_Nn0YURdl6GmZBv3qw",
-  "ChIJF-b9NNxL0YURK-n5t9R5Ac0",
-  "ChIJxS1vG8Jn0YURch5qw5JEDVc",
-  "ChIJ6c6y8YZm0YURrVyS6Ug4zQg",
-  "ChIJj3FZdJPl0YUR83rmC6muZgA",
-  "ChIJn0zUfH3_0YURn1aCqV9GJ0k",
-  "ChIJv_qP_JtK0YURkhhYmkvU7iQ",
-  "ChIJu3-8zjrp0YURL6wtveu5s9o",
-  "ChIJ6Vx5cONo0YURDseE5xQ6t4w",
-  "ChIJwUeazJcF0YURDjuNkk0iJ6c",
-  "ChIJh-4JYldt0YUR-yhto6vRw1I",
-  "ChIJs5cYZVf_0YURNzvc1__rlLw",
-  "ChIJ6R-VQJL_0YURjjDKq0-Qh5I",
-  "ChIJnVThgJ7b0YURQ97yjM3i4x8",
-  "ChIJNc2jU5_y0YURBxsV-6zjCTI",
-  "ChIJn6fs8ob_0YURsKzcJhtvm9A",
-  "ChIJcU4yw0jN0oUR8s2Uqklqlk4",
-  "ChIJu0RvGKq_0YURbcxlK1-qE3E",
-  "ChIJZdGm_ze_0YUR8REe0K77p9Q",
-  "ChIJJ-UvrZO_0YURdr68OBo-s94",
-  "ChIJ04U2ekD_0YURadWs31DPSgg",
-  "ChIJIY6Kc4v_0YURrOYiYJ3jhBs",
-  "ChIJH6bzZJf_0YUR_zQ9JUGzLd8",
-  "ChIJf8sB1S_10YURnGB4YPmPy0k",
-  "ChIJr2jYxPqF0YURLphKMnByUd4",
-  "ChIJFw1EeACv0YUR1cQ6c1ddr_c",
-  "ChIJ1cakfBX_0YURynZyxvCkT5M"
+  "ChIJtRKjR83-0YURSRoKZak2sSs",//Zocalo
+  "ChIJsT3Hgdb-0YURN-n3sjWWlUI"//Salto del agua
 ];
 
 
 //Declaramos la funcion encuentrame
-function encuentrame() {
-    //Si podemos utilizar la geolocalizacion entramos al if
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(function(position) {
-            
+async function encuentrame() {
+  return new Promise((resolve, reject) => {
+    setTimeout(function() {
+      //Si podemos utilizar la geolocalizacion entramos al if
+      if (navigator.geolocation){
+          navigator.geolocation.getCurrentPosition(function(position) {
+              
             //Obtenemos la latitud y la longitud del USUARIO
             latitud = position.coords.latitude
             longitud = position.coords.longitude
             userLocation = {lat: latitud, lng: longitud};
                 
-            // Pintamos el mapa en el documento HTML con los valores obtenidos de lat y lng
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: userLocation,
-                zoom: 12
-            });
+            map = new google.maps.Map(document.getElementById('map'));
+            map.setCenter(userLocation);
+            map.setZoom(12)
 
             //Colocar un marcador en la ubicación del usuario
             let marker = new google.maps.Marker({
@@ -72,72 +36,123 @@ function encuentrame() {
                 map: map,
                 title: 'Mi ubicación'
             });
-            
-        }, function() {
-            // Si hay un error al obtener la ubicación del usuario, mostrar un mensaje de error en la consola
-                console.log('Error: The Geolocation service failed.');
-            });
-    }
-
-    else {
-        //Si el navegador no admite la API Geolocation, mostrar un mensaje de error en la consola
-        console.log('Error: Your browser doesn\'t support geolocation.');
-    }
-
-    obtenerLugaresCercanos()
-}
-
-function obtenerLugaresCercanos(){
-  // Crea una instancia del servicio de Places
-  const service = new google.maps.places.PlacesService(map);
-
-  gimnasiosIds.forEach((placeId) => {
-    service.getDetails(
-      {
-        placeId: placeId,
-      },
-      (place, status) => {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          //console.log(status)
-          // Calcula la distancia entre la ubicación del usuario y el lugar
-          const distance = calcularDistancia(userLocation, place.geometry.location);
-  
-          // Filtra los lugares que están a menos de 500 metros de distancia
-          if (distance <= 10000) {
-            // Agrega el lugar a tu lista de lugares cercanos
-            lugaresCercanos.push(place);
+              
+          }, function() {
+              // Si hay un error al obtener la ubicación del usuario, mostrar un mensaje de error en la consola
+                      console.log('Error: The Geolocation service failed.');
+                  });
           }
-        }
-      }
-    );
-  });
+          else {
+              //Si el navegador no admite la API Geolocation, mostrar un mensaje de error en la consola
+              console.log('Error: Your browser doesn\'t support geolocation.');
+          }
 
-  // Espera a que todas las solicitudes de detalles de lugares se completen antes de continuar
-  Promise.all(gimnasiosIds.map((placeId) => service.getDetails({ placeId })))
-    .then((results) => {
-      // Filtra los lugares que están a menos de 500 metros de distancia
-      const filteredResults = filtrarLugaresPorDistancia(results, 10000);
+          resolve();
 
-      // Muestra los lugares cercanos en la consola
-      console.log(filteredResults);
-    })
-    .catch((error) => {
-      console.error(error);
+      },1000);
     });
+    
 }
+
+async function obtenerLugaresCercanos(){
+  return new Promise((resolve, reject) => {
+    setTimeout(function(){
+      // Crea una instancia del servicio de Places
+      map = new google.maps.Map(document.getElementById('map'));
+      console.log(map)
+      
+      const service = new google.maps.places.PlacesService(map);
+
+      gimnasiosIds.forEach((placeId) => {
+        service.getDetails({placeId: placeId,},
+            (place, status) => {
+              if (status === google.maps.places.PlacesServiceStatus.OK) {
+                console.log(status)
+                // Calcula la distancia entre la ubicación del usuario y el lugar
+                console.log(userLocation);
+                const distance = calcularDistancia(userLocation, place.geometry.location);
+                
+                // //PINTAR EL LUGAR
+                // let marker = new google.maps.Marker({
+                //   position: {lat: place.geometry.location.lat(), lng:place.geometry.location.lng()},
+                //   map: map,
+                //   title: 'Gimansio'
+                // });
+
+                // Filtra los lugares que están a menos de 500 metros de distancia*****
+                if (distance <= 10000) {
+                  // Agrega el lugar a tu lista de lugares cercanos
+                  lugaresCercanos.push(place);
+                  console.log(lugaresCercanos)
+                }
+              }
+            }
+          );
+        });
+
+        resolve(lugaresCercanos);
+      });
+    }, 1000);   
+}
+
 
 // Definimos una función para calcular la distancia entre dos puntos en un plano
 function calcularDistancia(usuarioUbicacion, gimnasioUbicacion) {
-  const distanciaX = gimnasioUbicacion.lat - usuarioUbicacion.lat;
-  const distanciaY = gimnasioUbicacion.lng - usuarioUbicacion.lng;
+  console.log(
+    gimnasioUbicacion.lat(),
+    gimnasioUbicacion.lng(),
+    usuarioUbicacion.lat,
+    usuarioUbicacion.lng
+  )
+  const distanciaX = gimnasioUbicacion.lat() - usuarioUbicacion.lat;
+  const distanciaY = gimnasioUbicacion.lng() - usuarioUbicacion.lng;
   return Math.sqrt(distanciaX * distanciaX + distanciaY * distanciaY);
 }
 
-function filtrarLugaresPorDistancia(places, maximaDistancia) {
-  return places.filter((place) => {
-    const distance = calcularDistancia(userLocation, place.geometry.location);
-    return distance <= maximaDistancia;
+// function filtrarLugaresPorDistancia(places, maximaDistancia) {
+//   return places.filter((place) => {
+//     const distance = calcularDistancia(userLocation, place.geometry.location);
+//     return distance <= maximaDistancia;
+//   });
+// }
+
+async function pruebas(){
+  return new Promise((resolve, reject) => {
+    setTimeout(function(){
+      console.log('Terminando funcion obtener lugares cercanos');
+      lugaresCercanos.forEach((lugar) => {
+        console.log(lugar);
+      });
+      resolve();
+    }, 1000)
+   
   });
 }
 
-window.encuentrame = encuentrame;
+async function main (){
+  console.log('Iniciando main')
+  await encuentrame();
+  console.log('Ya se obtuvo la ubicacion del usuario');
+  await obtenerLugaresCercanos();
+  await pruebas();
+  
+}
+
+window.main = main;
+// window.addEventListener('load', main)
+
+//Primero identar el codigo(OPCIONAL)
+//Eliminar el filtrado por distancia
+//Buscar los elementos que tengan la menor distancia
+//{placeId: "", distancia: ""}
+//Agregar en una lista 
+//Ordenar esa lista del menor al mayor
+//Pintar los primeros 3 obetos de esa lista con un marcador diferenciado
+//Por color o forma lo que sea...
+
+//Crear conectividad entre pantallas
+//Reestilizar el mapa
+//Agregar la opcion de ver mas gimnasios
+
+//Simulacion de usuarios en el gimnasio 
+//Y determinar cual de los primeros 3 gimnasios es el mas bueno.
